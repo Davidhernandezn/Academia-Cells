005b7026fdb7ccb4a9e5d8e2c302343ed36bb816
Davidhernandezn <d4vidhernandezn@gmail.com>
2023-12-30 08:48:37 -0600

    ESTILOS CON PROPIEDADES PERZONALIZADAS HOST

--- 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
+++ 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
@@ -12,17 +12,35 @@ export class EjercicioSiete extends LitElement{
     //     `;
     // }
 
-static get styles(){
-    return[
-        stylesModule,
-        //PODEMOS AGREGAR ESTILOS CSS css``
-    ]
-}
+    //POR MODULO
+// static get styles(){
+//     return[
+//         stylesModule,
+//         //PODEMOS AGREGAR ESTILOS CSS css``
+//     ]
+// }
 
+//ESTILOS CON PROPIEDADES PERZONALIZADAS HOST
+static get styles(){
+    return css `
+    /**PODEMOS REUTILIZARLO EN OTROS COMPONENTES**/
+        :host{
+            --primary-color:blue;
+            --font-size: 3.2rem;
+            --pading-left:16px;
+        }
+
+        p{
+            color: var(--primary-color);
+            font-size: var(--font-size);
+            padding-left: var(--pading-left);
+        }
+    `
+}
 // render para llamar al template
     render(){
         return html`
-        <p>Ejercicio 7</p>
+        <p>Ejercicio 7 y 9</p>
             <h1>Estilos estaticos</h1>
 
             <button class="btn">Click</button>
