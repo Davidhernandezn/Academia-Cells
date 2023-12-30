ef3d17c3b36742a669cb5b9d1460ef017c8fb54d
Davidhernandezn <d4vidhernandezn@gmail.com>
2023-12-29 23:04:19 -0600

    Agregar estilos desde modulo

---
+++ 3-LitElement/LitElement/Ejercicio_07/css/styles.js
@@ -0,0 +1,9 @@
+import {css } from 'lit-element';
+
+export const stylesModule = css `
+    .btn{   
+        color:blue;     
+        width:90px;
+        background-color: red;
+    }
+`;
--- 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
+++ 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
@@ -1,20 +1,31 @@
 //IMPORTAR CSS
 import { LitElement, html, css } from 'lit-element';
+import { stylesModule } from '../css/styles';//IMPORTAR MODULO CREADO
+
 export class EjercicioSiete extends LitElement{
-    static get styles(){
-        const mainColor = css `brown`;
-        return css `
-            h1{
-                color: ${mainColor};
-            }
-        `;
-    }
+    // static get styles(){
+    //     const mainColor = css `brown`;
+    //     return css `
+    //         h1{
+    //             color: ${mainColor};
+    //         }
+    //     `;
+    // }
+
+static get styles(){
+    return[
+        stylesModule,
+        //PODEMOS AGREGAR ESTILOS CSS css``
+    ]
+}
 
 // render para llamar al template
     render(){
         return html`
         <p>Ejercicio 7</p>
             <h1>Estilos estaticos</h1>
+
+            <button class="btn">Click</button>
         `
     }
 }
