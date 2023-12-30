862fbd346ee1bdd6ab1673c058b5bf76fba7ee60
Davidhernandezn <d4vidhernandezn@gmail.com>
2023-12-30 09:22:38 -0600

    Manejo de Eventos: keyupEvent, inputEvent, EventChange

--- 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
+++ 3-LitElement/LitElement/Ejercicio_07/src/ejercicio-siete.js
@@ -46,11 +46,46 @@ static get styles(){
             <button class="btn">Click</button>
             <hr>
             <button class="btn" @click=${this.primerEvento}>Boton con Evento</button>
+            <hr>
+            <input type="text" placeholder="Input uno" id="inputUno" @change="${this.event}"><br><br>
+            <input type="text" placeholder="Input dos" id="inputDos" disabled><br><br>
+            <input type="text" placeholder="Input tres" id="inputTres" @keyup="${this.keyupEvent}"><br><br>
+            <input type="text" placeholder="Input cuatro" id="inputCuatro" @input="${this.inputEvent}"><br><br>
+
+
         `
     }
 
     primerEvento(){
         console.log('Diste Click')
+    }
+
+    //EVENTO CHANGE
+    event(e){
+        let valueInput = e.target.value;
+        let tamanio = valueInput.length;
+        console.log('EVENTO CHANGE',valueInput, tamanio);
+    }
+
+
+    //EVENTO KEYUP // CADA QUE UN VALOR ES MODIFICADO
+    keyupEvent(e){
+        if(e.target.value.length === 4){
+            this.shadowRoot.getElementById("inputDos").disabled = false;
+        }else{
+            this.shadowRoot.getElementById("inputDos").disabled = true;
+        }
+        console.log(e.target.value.length);
+    }
+
+    //EVENTO INPUT//DETECTA CADA CAMBI0
+    inputEvent(e){
+        if(e.target.value.length === 4){
+            console.log('Es igual a 4');
+        }else{
+            console.log('No es igual');
+        }
+
     }
 }
 // PARA QUE EL NAVEGAGOR PUEDA INTERPRETARLO
